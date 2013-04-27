682c4a87d5b6c12926af8fc1b95e1dbf59932f6f
Arto Bendiken <arto@bendiken.net>
2013-04-27 17:25:09 +0200

    Customized the Doxygen configuration file.

---
+++ doc/doxygen/mainpage.h
@@ -0,0 +1,8 @@
+/**
+ * @mainpage librfc API Documentation
+ *
+ * This is API documentation automatically generated from the source code
+ * using the Doxygen documentation generation tool.
+ *
+ * @see http://librfc.org/
+ */
