a7bd55c0eb18b81e72659698bb4f8a3db360e8a8
Arto Bendiken <arto@bendiken.net>
2013-04-28 01:37:11 +0200

    Defined the rfc1321::md5 structure.

--- src/rfc/rfc1321/md5.h
+++ src/rfc/rfc1321/md5.h
@@ -3,16 +3,71 @@
 #ifndef RFC1321_MD5_H
 #define RFC1321_MD5_H
 
+#include <cstdint> /* for std::uint8_t */
+#include <utility> /* for std::swap() */
+
 /**
  * @file
  */
 
-#ifdef __cplusplus
-extern "C" {
-#endif
+namespace rfc1321 {
+  struct md5;
+}
 
-#ifdef __cplusplus
-} /* extern "C" */
-#endif
+/**
+ * Represents an MD5 digest.
+ */
+struct rfc1321::md5 {
+protected:
+  std::uint8_t _data[16] = {};
+
+public:
+  /**
+   * Default constructor.
+   */
+  md5() noexcept {}
+
+  /**
+   * Constructor.
+   */
+  md5(const std::uint8_t* const data) noexcept {
+    *_data = *data;
+  }
+
+  /**
+   * Copy constructor.
+   */
+  md5(const md5& other) noexcept {
+    *_data = *other._data;
+  }
+
+  /**
+   * Move constructor.
+   */
+  md5(md5&& other) noexcept {
+    std::swap(_data, other._data);
+  }
+
+  /**
+   * Destructor.
+   */
+   ~md5() noexcept {}
+
+  /**
+   * Copy assignment operator.
+   */
+  md5& operator=(md5 other) noexcept {
+    std::swap(_data, other._data);
+    return *this;
+  }
+
+  /**
+   * Move assignment operator.
+   */
+  md5& operator=(md5&& other) noexcept {
+    std::swap(_data, other._data);
+    return *this;
+  }
+};
 
 #endif /* RFC1321_MD5_H */
