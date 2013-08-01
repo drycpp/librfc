a3b5fbd877abbe15a0dc3052be38486afa973d3c
Arto Bendiken <arto@bendiken.net>
2013-08-01 03:17:00 +0200

    Imported stubs for per-module example programs.

---
+++ doc/examples/md5.cc
@@ -0,0 +1,9 @@
+/* This is free and unencumbered software released into the public domain. */
+
+#include <cstdlib>
+
+#include <rfc/md5.h>
+
+int main() {
+  return 0; // TODO
+}
---
+++ doc/examples/uuid.cc
@@ -0,0 +1,9 @@
+/* This is free and unencumbered software released into the public domain. */
+
+#include <cstdlib>
+
+#include <rfc/uuid.h>
+
+int main() {
+  return 0; // TODO
+}
