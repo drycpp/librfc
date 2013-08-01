896261dc3b4ae0026b934a5fc1cfc574bb3ca6d3
Arto Bendiken <arto@bendiken.net>
2013-08-01 02:34:14 +0200

    Defined the rfc3174::sha1 structure.

--- src/rfc/rfc3174/sha1.cc
+++ src/rfc/rfc3174/sha1.cc
@@ -5,3 +5,24 @@
 #endif
 
 #include "sha1.h"
+
+#include <cstring> /* for std::strlen() */
+
+using namespace rfc3174;
+
+static_assert(sizeof(sha1) == sha1::size, "sizeof(rfc3174::sha1) != rfc3174::sha1::size");
+
+sha1
+sha1::compute(const char* const data) noexcept {
+  return compute(reinterpret_cast<const std::uint8_t*>(data), std::strlen(data));
+}
+
+sha1
+sha1::compute(const std::uint8_t* const data,
+              const std::size_t size) noexcept {
+  sha1 digest;
+
+  (void)data, (void)size; // TODO
+
+  return digest;
+}
--- src/rfc/rfc3174/sha1.h
+++ src/rfc/rfc3174/sha1.h
@@ -7,12 +7,200 @@
  * @file
  */
 
-#ifdef __cplusplus
-extern "C" {
-#endif
+#include <cstddef> /* for std::size_t */
+#include <cstdint> /* for std::uint8_t */
+#include <cstring> /* for std::memcmp(), std::memset() */
+#include <utility> /* for std::swap() */
 
-#ifdef __cplusplus
-} /* extern "C" */
-#endif
+namespace rfc3174 {
+  struct sha1;
+}
+
+/**
+ * Represents a SHA-1 digest.
+ */
+struct rfc3174::sha1 {
+public:
+  static constexpr std::size_t size = 20; /* 20 bytes (160 bits) */
+
+  /**
+   * Computes the SHA-1 digest of the given NUL-terminated input string.
+   */
+  static sha1 compute(const char* data) noexcept;
+
+  /**
+   * Computes the SHA-1 digest of the given input data.
+   */
+  static sha1 compute(const std::uint8_t* data, std::size_t size) noexcept;
+
+  /**
+   * Default constructor. The structure is all zeroes after construction.
+   */
+  sha1() noexcept {}
+
+  /**
+   * Constructor.
+   */
+  sha1(const std::uint8_t* const data) noexcept {
+    *_data = *data;
+  }
+
+  /**
+   * Copy constructor.
+   */
+  sha1(const sha1& other) noexcept {
+    *_data = *other._data;
+  }
+
+  /**
+   * Move constructor.
+   */
+  sha1(sha1&& other) noexcept {
+    std::swap(_data, other._data);
+  }
+
+  /**
+   * Destructor.
+   */
+  ~sha1() noexcept {}
+
+  /**
+   * Copy assignment operator.
+   */
+  sha1& operator=(sha1 other) noexcept {
+    std::swap(_data, other._data);
+    return *this;
+  }
+
+  /**
+   * Move assignment operator.
+   */
+  sha1& operator=(sha1&& other) noexcept {
+    std::swap(_data, other._data);
+    return *this;
+  }
+
+  /**
+   * Equality operator.
+   */
+  bool operator==(const sha1& other) const {
+    return compare(other) == 0;
+  }
+
+  /**
+   * Inequality operator.
+   */
+  bool operator!=(const sha1& other) const {
+    return compare(other) != 0;
+  }
+
+  /**
+   * Less-than operator.
+   */
+  bool operator<(const sha1& other) const {
+    return compare(other) < 0;
+  }
+
+  /**
+   * Less-than-or-equal-to operator.
+   */
+  bool operator<=(const sha1& other) const {
+    return compare(other) <= 0;
+  }
+
+  /**
+   * Greater-than operator.
+   */
+  bool operator>(const sha1& other) const {
+    return compare(other) > 0;
+  }
+
+  /**
+   * Greater-than-or-equal-to operator.
+   */
+  bool operator>=(const sha1& other) const {
+    return compare(other) >= 0;
+  }
+
+  /**
+   * Returns a reference to the byte at the given position.
+   */
+  std::uint8_t& operator[](const std::size_t position) noexcept {
+    return _data[position];
+  }
+
+  /**
+   * Returns a reference to the byte at the given position.
+   */
+  const std::uint8_t& operator[](const std::size_t position) const noexcept {
+    return _data[position];
+  }
+
+  /**
+   * Returns a pointer to the digest data.
+   */
+  std::uint8_t* data() {
+    return _data;
+  }
+
+  /**
+   * Returns a pointer to the digest data.
+   */
+  const std::uint8_t* data() const {
+    return _data;
+  }
+
+  /**
+   * Returns a reference to the first byte of the digest data.
+   */
+  std::uint8_t& front() {
+    return operator[](0);
+  }
+
+  /**
+   * Returns a reference to the first byte of the digest data.
+   */
+  const std::uint8_t& front() const {
+    return operator[](0);
+  }
+
+  /**
+   * Returns a reference to the last byte of the digest data.
+   */
+  std::uint8_t& back() {
+    return operator[](size - 1);
+  }
+
+  /**
+   * Returns a reference to the last byte of the digest data.
+   */
+  const std::uint8_t& back() const {
+    return operator[](size - 1);
+  }
+
+  /**
+   * Compares this digest to the given other digest.
+   */
+  inline int compare(const sha1& other) const noexcept {
+    return std::memcmp(_data, other._data, sizeof(_data));
+  }
+
+  /**
+   * Clears the digest data to be all zeroes.
+   */
+  void clear() noexcept {
+    std::memset(_data, 0, sizeof(_data));
+  }
+
+  /**
+   * Exchanges the digest data with the given other digest.
+   */
+  void swap(sha1& other) noexcept {
+    std::swap(_data, other._data);
+  }
+
+protected:
+  std::uint8_t _data[size] = {};
+};
 
 #endif /* RFC3174_SHA1_H */
