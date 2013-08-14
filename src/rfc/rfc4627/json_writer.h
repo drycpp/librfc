992db6e1b3c6027d14e9af15314d1f9e6a77d77a
Arto Bendiken <arto@bendiken.net>
2013-08-14 20:31:13 +0200

    Defined the json_writer class.

--- src/rfc/rfc4627/json_writer.h
+++ src/rfc/rfc4627/json_writer.h
@@ -7,9 +7,35 @@
  * @file
  */
 
+#include <cstdio> /* for FILE */
+
 namespace rfc4627 {
-  class json_writer {
-  };
+  class json_writer;
 }
+
+/**
+ * A simple JSON serializer.
+ */
+class json_writer {
+public:
+  json_writer(FILE* stream) : _stream(stream) {}
+
+protected:
+  inline void write(const int c) {
+    fputc(c, _stream);
+  }
+
+  inline void write(const char* const s) {
+    fputs(s, _stream);
+  }
+
+  enum class state {
+    none = 0,
+  };
+
+private:
+  state _state  = state::none;
+  FILE* _stream = nullptr;
+};
 
 #endif /* RFC_RFC4627_JSON_WRITER_H */
